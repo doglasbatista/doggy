96167138325f6af3250d30edddca1484c4c50a73
Doglas <doglasbatista@gmail.com>
2020-09-10 22:20:43 -0300

    Install and config stitches

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {reset} from 'stitches-reset';
+
+import {css} from './config/stitches';
+
+css.global(reset);
 
 function App() {
-  return (
-    <div className="App">
-      Doggy
-    </div>
-  );
+  return <div className="App">Doggy</div>;
 }
 
 export default App;
